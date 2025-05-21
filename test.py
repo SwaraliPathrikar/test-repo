d1075d86e7f79b6a88d29958291e101ae1249c60
SwaraliPathrikar <sw04spathrikar@gmail.com>
2025-05-21 17:21:49 +0530

    this is commented

--- test.py
+++ test.py
@@ -1,1 +1,17 @@
 print('hello')
+
+'''
+to initialize new git repo, to track changes -> git init .
+to add the changes in repo -> git add .
+to commit and finalise the changes -> git commit "msg"
+
+to see the commit hashes -> git log
+to go and just see the prev versions -> git checkout <hash-id> atleast 4 chars
+to move to the specific checkpoint/version -> git reset --hard <hashid>
+
+to coonect local git repo to github repo -> git remote add origin <repo link> https://github.com/SwaraliPathrikar/test-repo.git
+
+to push code to github -> git push origin master (whatever we named to repo branch)
+
+to pull code from github, git pull
+'''
